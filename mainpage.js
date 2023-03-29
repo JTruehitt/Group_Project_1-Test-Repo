a5cfee1b7bc3a75b15920edd8fe19ff0e1f597d3
JTrue <truehitt@gmail.com>
2023-03-29 15:49:09 -0400

    added main page to select which path you'd like to go

---
+++ mainpage.js
@@ -0,0 +1,15 @@
+let OTDBBtn = $(".OTDBBtn");
+let triviaAPIBtn = $(".triviaAPIBtn");
+let marvelBtn = $(".marvelBtn");
+
+OTDBBtn.click(() => {
+  location.assign("OTDB.html");
+});
+
+triviaAPIBtn.click(() => {
+  location.assign("TriviaAPI.html");
+});
+
+marvelBtn.click(() => {
+  location.assign("marvel.html");
+});
