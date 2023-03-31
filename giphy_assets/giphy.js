cc8e517e953a7994622ae3980fcfcbce55581251
JTrue <truehitt@gmail.com>
2023-03-31 15:07:37 -0400

    built out giphy query to integrate

--- giphy_assets/giphy.js
+++ giphy_assets/giphy.js
@@ -1,13 +1,13 @@
 let giphyDisplay = $(".giphyDisplay")
 let searchTerm = $(".searchTerm")
 
-let apiKey = "api_key=axJ1DtkLXB8rYsm7KvxDxCwLhNlzccGq";
 
-let baseURL = "https://api.giphy.com/v1/gifs/search?";
-
-let rating = "&rating=pg-13"
 
 function queryGiphy(query) {
+
+    let apiKey = "api_key=axJ1DtkLXB8rYsm7KvxDxCwLhNlzccGq";
+    let baseURL = "https://api.giphy.com/v1/gifs/search?";
+    let rating = "&rating=pg-13"
 
     fetch(baseURL + apiKey + rating + "&q=" + query)
     .then(res => res.json())
@@ -17,18 +17,36 @@ function queryGiphy(query) {
     })
 }
 
-
 function renderGifs(data) {
 giphyDisplay.empty();
 let gif = $("<img>");
-let gifURL = data.data[1].images.original.url;
+let i = Math.floor(Math.random() * data.data.length)
+let gifURL = data.data[i].images.original.url;
 console.log(gifURL)
 gif.attr("src", gifURL);
+gif.addClass("gifIMG")
 giphyDisplay.append(gif);
 }
 
+function pickQuery(result) {
+    const correct = ["correct", "way+to+go", "good+job"];
+    const incorrect = ["incorrect", "wrong", "nope"];
+    let r = Math.floor(Math.random() * correct.length);
+    if (result === 0) {
+        query = correct[r];
+        queryGiphy(query);
+    } else {
+        query = incorrect[r];
+        queryGiphy(query);
+    }
+}
+
+
+
 
 $(".startGameBtn").click(() => {
-    let query = searchTerm.val().trim().replace(" ","+");
-    queryGiphy(query);
+    // let query = searchTerm.val().trim().replace(" ","+");
+    // queryGiphy(query);
+
+    pickQuery(1);
 })
